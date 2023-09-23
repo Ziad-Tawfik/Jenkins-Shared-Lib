32f39294f091753461737c01b551eb98c21af5c0
Ziad-Tawfik <132526715+Ziad-Tawfik@users.noreply.github.com>
2023-09-24 01:18:29 +0300

    Create install_nginx.groovy

---
+++ vars/install_nginx.groovy
@@ -0,0 +1,24 @@
+def call(){
+  pipeline {
+    agent { label 'EC2-Agent-1' }
+
+    stages {
+        stage('Install-Nginx') {
+            steps {
+                sh "sudo yum -y update"
+                sh "sudo yum install -y nginx"
+            }
+        }
+        stage('Start-Nginx') {
+            steps {
+                sh "sudo systemctl enable --now nginx"
+            }
+        }
+        stage('Test-Nginx') {
+            steps {
+                sh "curl localhost"
+            }
+        }
+    }
+  }
+}
